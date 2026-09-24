15e1653d8e9e3c630cecb71df24ff221654f88c7
125052608-stack <125052608@upq.edu.mx>
2026-09-23 23:12:20 -0600

    correciones a codigo de caja
    se quito el console y se retornaron valores para usarlos en cliente.js, se saco subtotal en vez de precio para reducir arreglo

--- CooffeeCode/caja.js
+++ CooffeeCode/caja.js
@@ -3,6 +3,7 @@ let totalAcumulado = 0;
 
 function agregarPedido(producto, precio,cantidad) {
   const subtotal = precio * cantidad;
+  // se usa destructing aqui
   listadePedidos.push({ producto, precio, cantidad, subtotal});
 
   totalAcumulado += subtotal;
@@ -17,8 +18,30 @@ function obtenerTotal() {
   return totalAcumulado;
 }
 
-module.exports = {
-  agregarPedido,
-  obtenerPedidos,
-  obtenerTotal
-};
+/* PARTE DOS 
+
+destructing: {precio} en vez de poner objeto p y .precio
+
+*/
+
+
+function caja() {
+  const subtotal = listadePedidos.reduce((acum, { subtotal }) => acum + subtotal, 0);
+  const iva = subtotal * 0.16;
+  const total = subtotal + iva;
+
+  const listaPedidos = listadePedidos.map(({ producto, precio, cantidad, subtotal }) => ({
+    producto,
+    precio,
+    cantidad,
+    subtotal
+  }));
+  
+  return {
+    listaPedidos,
+    subtotal,
+    iva,
+    total
+  }
+}
+module.exports = {agregarPedido,obtenerPedidos,obtenerTotal,caja};
