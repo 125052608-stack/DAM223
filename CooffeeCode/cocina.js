bf13f43d7317f0675095898ffd997f16a5905b51
125052608-stack <125052608@upq.edu.mx>
2026-09-23 23:13:03 -0600

    correciones a codigo de cocina
    se corrigió el console

--- CooffeeCode/cocina.js
+++ CooffeeCode/cocina.js
@@ -1,7 +1,8 @@
 let menu = [
     {producto: "café", precio: 50},
     {producto: "moka frappuccino", precio: 40},
-    {producto: "cheescake", precio: 80}
+    {producto: "cheescake", precio: 80},
+    {producto: "pastel", precio: 50}
 ];
 
 function mostrar() {
@@ -21,4 +22,32 @@ function editaryBorrar() {
 
 }
 
-module.exports = {menu, mostrar, editaryBorrar, agregarProducto};
+/* PARTE DOS 
+
+for each: devuelve undefined en arreglos exportados
+map: devuelve un nuevo arreglo con los resultados 
+
+*/
+
+function buscarPostres(){
+    let postres = menu.filter(p => p.tipo == "postres");
+    return postres.map(p => console.log(p.producto + ": $" + p.precio));
+        
+}
+
+function buscarBebidas() {
+        let bebidas = menu.filter(p => p.tipo == "bebidas");
+        return bebidas.map(p => console.log(p.producto + ": $" + p.precio));
+}
+
+function productosBaratos() {
+        let baratos = menu.filter (p => p.precio <  80)
+        return baratos.map  (p => console.log(p.producto + " $" + p.precio));
+}
+
+function productosCaros() {
+        let caros = menu.filter (p => p.precio >= 80 )
+        return caros.map(p => console.log(p.producto + "$" + p.precio));
+}
+
+module.exports = {menu, mostrar, editaryBorrar, agregarProducto, buscarPostres, buscarBebidas, productosBaratos, productosCaros};
