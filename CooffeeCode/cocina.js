579baaca146d07504261a338d7bb1c932e4a0dcc
125052608-stack <125052608@upq.edu.mx>
2026-09-25 10:17:20 -0600

    se agrego un método

--- CooffeeCode/cocina.js
+++ CooffeeCode/cocina.js
@@ -1,9 +1,12 @@
 let menu = [
-    {producto: "café", precio: 50},
-    {producto: "moka frappuccino", precio: 40},
-    {producto: "cheescake", precio: 80},
-    {producto: "pastel", precio: 50}
+    {producto: "café", precio: 50, tipo: "bebidas",estado: "listo"},
+    {producto: "moka frappuccino", precio: 40, tipo: "bebidas", estado: "faltaIngrediente"},
+    {producto: "cheescake", precio: 80, tipo: "postres",estado: "errorC"},
+    {producto: "pastel", precio: 50,tipo: "postres",estado: "faltaIngrediente"}
+
 ];
+
+
 
 function mostrar() {
     for (let i = 0; i < menu.length; i++) {
@@ -13,13 +16,14 @@ function mostrar() {
 }
 
 function agregarProducto(nombre, precio) {
+    // agregar tipo
     menu.push({producto: nombre, precio: precio});
 }
 
 function editaryBorrar() {
     menu[1].precio = 75;
     menu.splice(0, 1);
-
+        //SPLICE: Borra parte de arreglo 
 }
 
 /* PARTE DOS 
@@ -31,23 +35,74 @@ map: devuelve un nuevo arreglo con los resultados
 
 function buscarPostres(){
     let postres = menu.filter(p => p.tipo == "postres");
-    return postres.map(p => console.log(p.producto + ": $" + p.precio));
+    return postres.map(p => (p.producto + ": $" + p.precio));
         
 }
 
 function buscarBebidas() {
         let bebidas = menu.filter(p => p.tipo == "bebidas");
-        return bebidas.map(p => console.log(p.producto + ": $" + p.precio));
+        return bebidas.map(p => (p.producto + ": $" + p.precio));
 }
 
 function productosBaratos() {
         let baratos = menu.filter (p => p.precio <  80)
-        return baratos.map  (p => console.log(p.producto + " $" + p.precio));
+        return baratos.map  (p => (p.producto + " $" + p.precio));
 }
 
 function productosCaros() {
         let caros = menu.filter (p => p.precio >= 80 )
-        return caros.map(p => console.log(p.producto + "$" + p.precio));
+        return caros.map(p => (p.producto + " $" + p.precio));
 }
+        /* Parte tres */
 
-module.exports = {menu, mostrar, editaryBorrar, agregarProducto, buscarPostres, buscarBebidas, productosBaratos, productosCaros};
+
+    function procesoCocina(nombreProd){
+            const prod = menu.find(prodprocesado => prodprocesado.producto === nombreProd);
+
+            if(!prod){
+                return Promise.reject("El producto no está en menú");
+            }
+            if(prod.estado == "errorC"){
+                return errorCocina(prod.estado);
+            } else if (prod.estado == "faltaIngrediente"){
+                return faltaIngrediente(prod.estado);
+            } else {
+                return Promise.resolve("Producto en proceso correcto");
+            }
+        }
+function errorCocina(estado) {
+      return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+
+            if (estado == "errorC") {
+                reject("Error en cocina");
+            } else {
+                resolve("No hubo errores en cocina");
+
+            }
+
+            }, 1000);
+        });
+      }
+
+      function faltaIngrediente(estado) {
+
+        return new Promise(function(resolve, reject) {
+            setTimeout(function() {
+
+                if (estado == "faltaIngrediente") {
+                    reject("Falta un ingrediente para poder preparar el café");
+                    
+                    } else {
+                        resolve("Ingredientes disponibles");
+                    }
+                }, 1000);
+            });
+        }
+
+        
+
+module.exports = {menu, mostrar, editaryBorrar, 
+    agregarProducto, buscarPostres, buscarBebidas, 
+    productosBaratos, productosCaros,
+    faltaIngrediente,procesoCocina,errorCocina};
